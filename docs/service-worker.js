f067cd1d7c7a29d97ca14796fea0366587125274
linkevery2s <legend1world@gmail.com>
2018-09-26 11:11:41 +0900

    up

---
+++ docs/service-worker.js
@@ -0,0 +1,10 @@
+self.addEventListener('install', function(e) {
+  console.log('[ServiceWorker] Install');
+});
+
+self.addEventListener('activate', function(e) {
+  console.log('[ServiceWorker] Activate');
+});
+
+// Œ»ó‚Å‚ÍA‚±‚Ìˆ—‚ğ‘‚©‚È‚¢‚ÆService Worker‚ª—LŒø‚Æ”»’è‚³‚ê‚È‚¢‚æ‚¤‚Å‚·
+self.addEventListener('fetch', function(event) {});
