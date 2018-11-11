9eeec2ab89428b8dfa7433b4a603175d8c9ad7ea
linkevery2s <legend1world@gmail.com>
2018-11-11 13:50:06 +0900

    up

--- docs/test/js/info.js
+++ docs/test/js/info.js
@@ -5,7 +5,7 @@ $(function(){
 	
 	//HTMLを生成
 	$.getJSON("https://linkevery2s.github.io/tklink/test/json/data.json", function(data){
-		$(data).each(function(){
+		$(data.release).each(function(){
 			$('<tr>'+
 			  '<th>'+this.day+'</th>'+
 			  '<td class="label"><span class="' + this.label + '">' + this.category + '</span></td>'+
